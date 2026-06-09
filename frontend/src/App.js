780b5d4027398c063fdca47447e31a0dd1ee94ef
Harish Naidu BN <harishbnpvg@gmail.com>
2026-06-09 13:27:25 +0530

    Fix duplicate React import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { pdfjs } from "react-pdf";
 import "bootstrap/dist/css/bootstrap.min.css";
